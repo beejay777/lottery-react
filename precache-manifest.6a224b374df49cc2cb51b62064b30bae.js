91fbb5013d6d6e91914eac2cd7d47bdac25fb9e8
Aquila Serpens <virus@localhost.localdomain>
2019-01-26 21:24:38 +0545

    Updates

---
+++ precache-manifest.6a224b374df49cc2cb51b62064b30bae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lottery-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "08fdf50af4c86ab78a6e",
+    "url": "/lottery-react/static/js/runtime~main.08fdf50a.js"
+  },
+  {
+    "revision": "b604a7fec35a0c7d28b2",
+    "url": "/lottery-react/static/js/main.b604a7fe.chunk.js"
+  },
+  {
+    "revision": "1088699f789ba065aa80",
+    "url": "/lottery-react/static/js/1.1088699f.chunk.js"
+  },
+  {
+    "revision": "b604a7fec35a0c7d28b2",
+    "url": "/lottery-react/static/css/main.056b89eb.chunk.css"
+  },
+  {
+    "revision": "e2dd80af57bf4e5bf12494f646d44304",
+    "url": "/lottery-react/index.html"
+  }
+];
